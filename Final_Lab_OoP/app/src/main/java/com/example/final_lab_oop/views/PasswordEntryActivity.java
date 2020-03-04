abcd689335561e4e6f9ba455401a372dd303983e
Keen12345 <60484944+Keen12345@users.noreply.github.com>
2020-03-04 13:58:33 -0700

    Views Package

--- Final_Lab_OoP/app/src/main/java/com/example/final_lab_oop/views/PasswordEntryActivity.java
+++ Final_Lab_OoP/app/src/main/java/com/example/final_lab_oop/views/PasswordEntryActivity.java
@@ -1,14 +1,16 @@
-package com.example.final_lab_oop;
+package com.example.final_lab_oop.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CreateEntryActivity extends AppCompatActivity {
+import com.example.final_lab_oop.R;
+
+public class PasswordEntryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_entry);
+        setContentView(R.layout.activity_password_entry);
     }
 }
--- Final_Lab_OoP/app/src/main/java/com/example/final_lab_oop/views/UserMenuActivity.java
+++ Final_Lab_OoP/app/src/main/java/com/example/final_lab_oop/views/UserMenuActivity.java
@@ -1,14 +1,16 @@
-package com.example.final_lab_oop;
+package com.example.final_lab_oop.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainMenuActivity extends AppCompatActivity {
+import com.example.final_lab_oop.R;
+
+public class UserMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_user_menu);
     }
 }
