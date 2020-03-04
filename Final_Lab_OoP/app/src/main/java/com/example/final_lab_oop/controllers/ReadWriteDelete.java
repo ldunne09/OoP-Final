b39b665e6a37e02c4d1a0378b935c8545bf9f71e
HiSanchezSrc <hisanchez@student.neumont.edu>
2020-03-04 14:44:28 -0700

    frick

--- Final_Lab_OoP/app/src/main/java/com/example/final_lab_oop/controllers/ReadWriteDelete.java
+++ Final_Lab_OoP/app/src/main/java/com/example/final_lab_oop/controllers/ReadWriteDelete.java
@@ -1,11 +1,39 @@
 package com.example.final_lab_oop.controllers;
 
-public class ReadWriteDelete {
+import com.example.final_lab_oop.models.*;
+
+public class ReadWriteDelete implements java.io.Serializable {
+
+    public static void write(User user) {
+        user.setName();
+        user.setPassword();
+        user.setExp();
+        user.setLvl();
+    }
+
+    public static void read() {
+
+    }
+
+    public static void delete() {
+
+    }
 
 
-    public static void read(){}
-
-    public void write(){}
-
-    public void delete(){}
 }
+//    private void buttonReadFile_onClick(View view) {
+//        try {
+//            StringBuilder result = new StringBuilder();
+//            String line;
+//
+//            String folder = getApplication().getFilesDir().getAbsolutePath() + File.separator + editTextFolderName.getText().toString();
+//            File subFolder = new File(folder);
+//            BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(folder, editTextFileName.getText().toString())));
+//            while ((line = bufferedReader.readLine()) != null) {
+//                result.append(line);
+//            }
+//            editTextContent.setText(result.toString());
+//        } catch (Exception e) {
+//            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+//        }
+//    }
--- Final_Lab_OoP/app/src/main/java/com/example/final_lab_oop/controllers/Users.java
+++ Final_Lab_OoP/app/src/main/java/com/example/final_lab_oop/controllers/Users.java
@@ -3,4 +3,6 @@ package com.example.final_lab_oop.controllers;
 public class Users {
 
     private String name;
+
+
 }
