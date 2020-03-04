00cf66c1ece35de17beb8d8d3d7bbddb95fdd088
Keen12345 <60484944+Keen12345@users.noreply.github.com>
2020-03-04 14:22:38 -0700

    Update Theme.java

--- Final_Lab_OoP/app/src/main/java/com/example/final_lab_oop/models/Theme.java
+++ Final_Lab_OoP/app/src/main/java/com/example/final_lab_oop/models/Theme.java
@@ -1,7 +1,13 @@
 package com.example.final_lab_oop.models;
 
+import android.graphics.Color;
+
 public class Theme {
 
+    public static final Color lightGray =
 
-    public void 
+    public int red(){
+         int hello = Color.red(255);
+         return hello;
+    }
 }
