c20d84888a7b506b7ca7e2376c02af59ac8f6b90
ldunne09 <60371058+ldunne09@users.noreply.github.com>
2020-03-05 14:34:29 -0700

    All the GUIS

---
+++ Final_Lab_OoP/app/src/main/java/com/example/final_lab_oop/views/LoadEntry_activity.java
@@ -0,0 +1,16 @@
+package com.example.final_lab_oop.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.final_lab_oop.R;
+
+public class LoadEntry_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_entry_activity);
+    }
+}
